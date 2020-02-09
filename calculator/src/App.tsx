240095cef6744799aa81703747d555b5888c2122
HaQadosch Berraka <haqadosch@pm.me>
2020-02-09 18:57:05 +0000

    ui: Add layout
    Create the overall Layout plus the amount duration mock up.

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { LoanSetting } from './LoanSetting';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>
+          Your loan repayment plans
+        </h1>
       </header>
-    </div>
+      <LoanSetting />
+      <article id="rcf-calculator">RCF repayment calculator</article>
+      <article id="bl-calculator">BL repayment calculator</article>
+      <footer>Footer, get to talk to a real person</footer>
+    </>
   );
 }
 
