57fa76fc77ec0e99e73b251d88a6627d9dff4953
HaQadosch Berraka <haqadosch@pm.me>
2020-02-10 00:18:25 +0000

    Updates

---
+++ precache-manifest.92122276da716230ffa0f17c4b8d02e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dddb8aa1b51314bcefd6b22a67fa8b7",
+    "url": "/loan-calculator/g/index.html"
+  },
+  {
+    "revision": "27e5184ae1d11f6f9acc",
+    "url": "/loan-calculator/g/static/css/main.cc47754d.chunk.css"
+  },
+  {
+    "revision": "be17a3cc2ceeaa208066",
+    "url": "/loan-calculator/g/static/js/2.a8a357e0.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/loan-calculator/g/static/js/2.a8a357e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e5184ae1d11f6f9acc",
+    "url": "/loan-calculator/g/static/js/main.bbe18781.chunk.js"
+  },
+  {
+    "revision": "29fc45697cabec7e3f98",
+    "url": "/loan-calculator/g/static/js/runtime-main.39a9526e.js"
+  }
+]);
